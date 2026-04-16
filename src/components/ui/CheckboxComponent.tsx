5ea9d82314d6df7588b9284228d6ee98313e78ea
anacmagalhaes <anacarolinasoaresmagalhaes@gmail.com>
2026-04-16 16:25:05 -0300

    feat: adicionado checkbox de termos e condições

---
+++ src/components/ui/CheckboxComponent.tsx
@@ -0,0 +1,21 @@
+import { Ionicons } from '@expo/vector-icons'
+import { Pressable, View } from "react-native"
+
+interface CheckboxProps {
+    checked: boolean,
+    onChange: (checked: boolean) => void
+}
+
+export const CheckboxComponent = ({ checked, onChange }: CheckboxProps) => {
+    return (
+        <Pressable onPress={() => onChange(!checked)}>
+            <View className={`w-6 h-6 rounded-md border items-center justify-center
+                ${checked ? "bg-[#299A68] border-[#299A68]" : "bg-transparent border-[#F3F4F6]"}
+                `}>
+                {checked && (
+                    <Ionicons name="checkmark" size={18} color="white" />
+                )}
+            </View>
+        </Pressable>
+    )
+}
